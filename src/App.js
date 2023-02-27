5c4b1f9b52d2bb57892638342b5bb8a5975fdf2d
dengoyoon <engus927@naver.com>
2023-02-27 18:44:50 +0900

    라우터 및 뼈대 제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import { Header } from "./shared/components";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">hi</header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
