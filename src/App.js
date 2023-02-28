aaabbd09e0729fabcbc3b8536a2e7574fec30c9c
dengoyoon <engus927@naver.com>
2023-02-28 16:33:44 +0900

    게임 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Game from "./pages/game";
 import { Header } from "./shared/components";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-      </Routes>
+      <section>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/game" element={<Game />}></Route>
+        </Routes>
+      </section>
     </BrowserRouter>
   );
 }
