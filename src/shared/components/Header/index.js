f9226ef4ab445825a8bdd7bb3b5dec1e5a29ed55
dengoyoon <engus927@naver.com>
2023-03-01 13:46:07 +0900

    헤더 퍼블리싱 완료

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "./header.scss";
 
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="container flex align-items-center">
+        <div className="header__logo">Luxury Dice!</div>
+        <button type="button" className="header__game-button">
+          Game
+        </button>
+        <button type="button" className="header__profile-button">
+          Profile
+        </button>
+      </div>
+    </div>
+  );
 }
