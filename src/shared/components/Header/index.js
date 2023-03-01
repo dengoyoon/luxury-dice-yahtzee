ae33ea569907deb65a0bf651cc8c0d8eddce268e
DuHyeon Yoon <engus927@naver.com>
2023-03-01 14:04:12 +0900

    feat(header) : 헤더 퍼블리싱 완료 (#1)
    * 헤더 퍼블리싱 완료
    
    * 버튼에 링크 추가, 버튼에 호버 색상 추가

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./header.scss";
 
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="container flex align-items-center">
+        <div className="header__logo">
+          <Link to="/">Luxury Dice 💫</Link>
+        </div>
+        <button type="button" className="header__game-button">
+          <Link to="/game">Game</Link>
+        </button>
+        <button type="button" className="header__profile-button">
+          Profile
+        </button>
+      </div>
+    </div>
+  );
 }
