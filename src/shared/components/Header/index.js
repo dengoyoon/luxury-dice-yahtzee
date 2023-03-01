acc4e0b5c7bfd5a1e1a29b5a23d3c7dd2576ecc9
dengoyoon <engus927@naver.com>
2023-03-01 14:00:34 +0900

    버튼에 링크 추가, 버튼에 호버 색상 추가

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./header.scss";
 
 export function Header() {
   return (
     <div className="header">
       <div className="container flex align-items-center">
-        <div className="header__logo">Luxury Dice!</div>
+        <div className="header__logo">
+          <Link to="/">Luxury Dice 💫</Link>
+        </div>
         <button type="button" className="header__game-button">
-          Game
+          <Link to="/game">Game</Link>
         </button>
         <button type="button" className="header__profile-button">
           Profile
