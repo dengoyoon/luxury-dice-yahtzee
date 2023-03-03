f0709b777e69019764b3189c44133c6250b2ff15
DuHyeon Yoon <engus927@naver.com>
2023-03-03 23:05:23 +0900

    feat(game/score-table) : 스코어 테이블 퍼블리싱 (#3)
    * 스코어 테이블 퍼블리싱 완료
    
    * turn state 추가

--- src/pages/game/components/ScoreTable/index.jsx
+++ src/pages/game/components/ScoreTable/index.jsx
@@ -1,54 +1,42 @@
 import React, { useState } from "react";
+import { upperSection, lowerSection } from "../../constants/scoreSections";
+
+import "./score-table.scss";
 
 const useTurn = (initTurn = 1) => {
   const [turn, setTurn] = useState(initTurn);
-  const addTurn = () => setTurn(turn + 1);
+  const increaseTurn = () => turn !== 13 ? setTurn(turn + 1) : 13;
 
-  return [turn, addTurn];
+  return [turn, increaseTurn];
 };
 
-const upperSection = [
-  "Aces",
-  "Twos",
-  "Threes",
-  "Fours",
-  "Fives",
-  "Sixes"
-];
-
-const lowerSection = [
-  "3 of a kind",
-  "4 of a kind",
-  "Full House",
-  "Small Straight",
-  "Large Straight",
-  "Yahtzee"
-];
-
 export function ScoreTable() {
-  const [turn, addTurn] = useTurn();
+  const [turn, increaseTurn] = useTurn();
   return (
-    <div>
-      <h3>Turn {turn}/13</h3>
+    <div className="paper">
+      <h2 className="turn">Turn {turn} / 13</h2>
       <table 
-        border={1}
+        className="score-table"
+        border={2}
+        cellPadding={10}
         cellSpacing={0}>
         <thead>
           <tr align="center">
-            <td></td>
-            <th>Me</th>
-            <th>Enemy</th>
+            <td width={110}></td>
+            <th width={50}>Me</th>
+            <th width={50}>Enemy</th>
           </tr>
         </thead>
         <tbody>
-          { [...upperSection, ...lowerSection].map(
+          {[...upperSection, ...lowerSection].map(
             (name, index) => (
               <tr key={index} align="center">
-                <th>{name}</th>
-                <td>3</td>
-                <td>1</td>
-              </tr>))
-            }
+                <th align="left">{name}</th>
+                <td></td>
+                <td></td>
+              </tr>)
+            )
+          }
         </tbody>
       </table>
     </div>
---
+++ src/pages/game/constants/scoreSections.js
@@ -0,0 +1,18 @@
+export const upperSection = [
+  "Aces",
+  "Twos",
+  "Threes",
+  "Fours",
+  "Fives",
+  "Sixes",
+];
+
+export const lowerSection = [
+  "3 of a kind",
+  "4 of a kind",
+  "Full House",
+  "Small Straight",
+  "Large Straight",
+  "Chance",
+  "Yahtzee",
+];
