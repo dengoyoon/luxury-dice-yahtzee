03bb7a0fe62f0f9b114eb66ea044f7d2fff7e772
dengoyoon <engus927@naver.com>
2023-03-03 20:13:57 +0900

    turn state 추가

--- src/pages/game/components/ScoreTable/index.jsx
+++ src/pages/game/components/ScoreTable/index.jsx
@@ -1,57 +1,42 @@
 import React, { useState } from "react";
+import { upperSection, lowerSection } from "../../constants/scoreSections";
+
 import "./score-table.scss";
 
 const useTurn = (initTurn = 1) => {
   const [turn, setTurn] = useState(initTurn);
-  const addTurn = () => setTurn(turn + 1);
+  const increaseTurn = () => turn !== 13 ? setTurn(turn + 1) : 13;
 
-  return [turn, addTurn];
+  return [turn, increaseTurn];
 };
 
-const upperSection = [
-  "Aces",
-  "Twos",
-  "Threes",
-  "Fours",
-  "Fives",
-  "Sixes"
-];
-
-const lowerSection = [
-  "3 of a kind",
-  "4 of a kind",
-  "Full House",
-  "Small Straight",
-  "Large Straight",
-  "Yahtzee"
-];
-
 export function ScoreTable() {
-  const [turn, addTurn] = useTurn();
+  const [turn, increaseTurn] = useTurn();
   return (
     <div className="paper">
       <h2 className="turn">Turn {turn} / 13</h2>
       <table 
         className="score-table"
-        border={1}
+        border={2}
         cellPadding={10}
         cellSpacing={0}>
         <thead>
           <tr align="center">
             <td width={110}></td>
-            <th width={60}>Me</th>
-            <th width={60}>Enemy</th>
+            <th width={50}>Me</th>
+            <th width={50}>Enemy</th>
           </tr>
         </thead>
         <tbody>
-          { [...upperSection, ...lowerSection].map(
+          {[...upperSection, ...lowerSection].map(
             (name, index) => (
               <tr key={index} align="center">
                 <th align="left">{name}</th>
                 <td></td>
                 <td></td>
-              </tr>))
-            }
+              </tr>)
+            )
+          }
         </tbody>
       </table>
     </div>
